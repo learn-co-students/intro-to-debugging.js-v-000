b7cbd8dc1826510bea2ccd3601416e8f5aed513e
Adam King <adam.king0126@gmail.com>
2016-03-14 12:02:15 -0500

    Done.

--- spec/intro_spec.js
+++ spec/intro_spec.js
@@ -1,15 +1,29 @@
 'use strict';
+var jasmine = require('jasmine-node');
+var jsdom = require('jsdom');
+var path = require('path');
 describe("Jasmine Walkthrough", function(){
+  var sayHey, sayHeyFriend;
+  beforeEach(function(done) {
+    var codeJs = path.resolve(__dirname, '..', 'code.js');
+    jsdom.env({
+      html: '<div></div>',
+      scripts: [codeJs],
+      onload: function(window) {
+        sayHey = window.sayHey;
+        sayHeyFriend = window.sayHeyFriend;
+        done();
+      }
+    });
+  });
   describe('#sayHey', function() {
-     it('should returns the string "hey friends"', function() {
-       expect(sayHey()).toBe("hey friends!");
-      });
-      it('should return a string', function() {
-        expect(sayHey()).toEqual(jasmine.any(String));
-      });
-   
-  }); 
-
+    it('should returns the string "hey friends"', function() {
+      expect(sayHey()).toBe("hey friends!");
+    });
+    it('should return a string', function() {
+      expect(sayHey()).toEqual(jasmine.any(String));
+    });
+  });
   describe('#sayHeyFriend', function(){
     it("should return the string 'hey friends _name_'", function (){
       expect(sayHeyFriend("kristin")).toBe("hey kristin!!")
@@ -18,6 +32,4 @@ describe("Jasmine Walkthrough", function(){
       expect(sayHeyFriend("kristin")).toEqual(jasmine.any(String))
     });
   });
-
-  
 });
