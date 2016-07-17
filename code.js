aee74f11cbd458b70c7b0b446b0c3e1667585a54
Savannah Scott <sascott624@gmail.com>
2016-07-17 15:05:33 +0000

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function sayHey(){
+  return "hey friends!";
+}
+
+function sayHeyFriend(name){
+  if (typeof name === "undefined"){
+    name = "Savannah";
+  }
+  return "hey " + name + "!!";
+}
